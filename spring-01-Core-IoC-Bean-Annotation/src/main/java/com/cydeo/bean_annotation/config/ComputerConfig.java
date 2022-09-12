0511157a6e76d3d74c4e301032690590de7acfaf
ozzy <ozzy@cydeo.com>
2022-09-12 12:43:31 -0400

    Stereotype Annotation

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
@@ -16,8 +16,16 @@ public class ComputerConfig {
 
     @Bean(name="sony")
     public Monitor monitorSony(){
+
         return new SonyMonitor("25 inch Beast","Sony",25);
     }
+
+    @Bean(name="sony2")
+    public Monitor monitorSony2(){
+
+        return new SonyMonitor("40 inch Beast","Sony",40);
+    }
+
 
     @Bean
     @Primary
