84117572040c2aa57e5537052ef62ab781a16f3e
ozzy <ozzy@cydeo.com>
2022-09-12 12:53:11 -0400

    Stereotype Annotation

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/ComputerTest.java
@@ -1,6 +1,7 @@
 package com.cydeo.streotype_annotation;
 
 import com.cydeo.streotype_annotation.config.PcConfig;
+import com.cydeo.streotype_annotation.monitorfactory.Monitor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,5 +9,8 @@ public class ComputerTest {
     public static void main(String[] args) {
 
         ApplicationContext container = new AnnotationConfigApplicationContext(PcConfig.class);
+
+        Monitor theMonitor = container.getBean(Monitor.class);
+        System.out.println(theMonitor.getSize());
     }
 }
