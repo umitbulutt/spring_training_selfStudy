9b79c3e447709eb0840a57780291ec11bc96a374
ozzy <ozzy@cydeo.com>
2022-09-12 16:05:53 -0400

    Autowiring

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
@@ -16,11 +16,12 @@ public class PC {
     private Monitor monitor;
     private Motherboard motherboard;
 
-    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
-        this.theCase = theCase;
-        this.monitor = monitor;
-        this.motherboard = motherboard;
-    }
+
+//    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+//        this.theCase = theCase;
+//        this.monitor = monitor;
+//        this.motherboard = motherboard;
+//    }
 
     public void powerUp() {
         theCase.pressPowerButton();
