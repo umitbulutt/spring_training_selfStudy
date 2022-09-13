483ab4f93ff7e0e549da4e4cc2a39ff4a9648eaf
ozzy <ozzy@cydeo.com>
2022-09-13 14:55:02 -0400

    Abstraction

--- spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -4,12 +4,18 @@ import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.proxy.EmailCommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
-import com.cydeo.repository.DBCommentRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommentService {
 
-    private CommentRepository commentRepository;
-    private CommentNotificationProxy commentNotificationProxy;
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
 
     public void publishComment(Comment comment){
         commentRepository.storeComment(comment);
