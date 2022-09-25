e5bec66a4c42a1833cccf32b033d5a90406a27b1
ozzy <ozzy@cydeo.com>
2022-09-25 15:57:28 -0400

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -19,9 +19,8 @@ public class StudentController {
     }
 
     @RequestMapping("/welcome") //localhost:8080/student/welcome?name=Ozzy
-    public String welcome(@RequestParam String name){
+    public String welcome(){
 
-        System.out.println(name);
 
         return "student/welcome";
     }
