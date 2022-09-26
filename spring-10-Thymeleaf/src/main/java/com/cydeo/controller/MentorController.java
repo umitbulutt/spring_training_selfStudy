9350404c40ac353b46e7155f8700968c35c24fc4
ozzy <ozzy@cydeo.com>
2022-09-26 15:06:08 -0400

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,6 +4,7 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -23,6 +24,13 @@ public class MentorController {
 
         return "mentor/mentor-register";
     }
+
+    @PostMapping("/confirm")
+    public String submitForm(){
+
+        return "mentor/mentor-confirmation";
+    }
+
 
 
 
