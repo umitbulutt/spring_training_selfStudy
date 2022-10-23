93d561c1e9ff67d12289af75569141ea770d3426
ozzy <ozzy@cydeo.com>
2022-10-23 15:30:27 -0400

    Spring Data

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Car.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Car.java
@@ -1,11 +1,17 @@
 package com.cydeo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@Data
 public class Car {
 
     @Id
@@ -14,4 +20,8 @@ public class Car {
     private String make;
     private String model;
 
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
 }
