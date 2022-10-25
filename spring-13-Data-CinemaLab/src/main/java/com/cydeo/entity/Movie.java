fd2bf6645aaa3c1ed92a7c121e681957218bbc25
ozzy <ozzy@cydeo.com>
2022-10-25 11:59:07 -0400

    Cinema Lab

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Movie.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Movie.java
@@ -1,10 +1,15 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.MovieState;
+import com.cydeo.enums.MovieType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
@@ -18,6 +23,12 @@ public class Movie extends BaseEntity {
     private Integer duration;
     @Column(columnDefinition = "text")
     private String summary;
+    @Enumerated(EnumType.STRING)
+    private MovieType type;
+    @Enumerated(EnumType.STRING)
+    private MovieState state;
+    private BigDecimal price;
+
 
 
 
