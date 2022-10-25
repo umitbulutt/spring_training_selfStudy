cc931132109d7939b27df448e62b561000ebf682
ozzy <ozzy@cydeo.com>
2022-10-25 12:18:14 -0400

    Cinema Lab

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Genre.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Genre.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor   //BREAK TILL 12:10
 @Data
 public class Genre extends BaseEntity {
 
