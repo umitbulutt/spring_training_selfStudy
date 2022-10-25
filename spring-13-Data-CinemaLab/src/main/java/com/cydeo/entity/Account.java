5fa1836eb760456cba5796c51e128714eaa41327
ozzy <ozzy@cydeo.com>
2022-10-25 13:10:48 -0400

    Cinema Lab

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Account.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Account.java
@@ -1,13 +1,15 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.UserRole;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "account_details")
 public class Account extends BaseEntity {
 
     private String name;
@@ -17,7 +19,8 @@ public class Account extends BaseEntity {
     private String city;
     private Integer age;
     private String postalCode;
-    private UserRole userRole;
-
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+    @OneToOne(mappedBy = "account")
     private User user;
 }
