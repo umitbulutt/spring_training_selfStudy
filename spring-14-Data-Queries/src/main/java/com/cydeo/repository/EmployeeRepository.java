62701282549dbbae545baecc294d469d41d2cbfc
ozzy <ozzy@cydeo.com>
2022-10-27 14:36:22 -0400

    Spring DATA Queries

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -0,0 +1,35 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+    //Display all employees with email address ""
+    List<Employee> findByEmail(String email);
+
+    //Display all employees with firstname "" and last name "",
+    //also show all employees with an email address ""
+    List<Employee> findByFirstNameAndLastNameOrEmail(String firstname,String lastname,String email);
+
+    //Display all employees that first name is not ""
+    List<Employee> findByFirstNameIsNot(String firstname);
+
+    //Display all employees where last name starts with ""
+    List<Employee> findByLastNameStartsWith(String pattern);
+
+    //Display all employees with salaries higher than ""
+    List<Employee> findBySalaryGreaterThan(Integer salary);
+
+    //Display all employees with salaries less than ""
+    List<Employee> findBySalaryLessThan(Integer salary);
+
+
+
+
+
+
+
+}
