ead43c30d065d2b7f440a18cf6d416f5bbb34fa7
Umit Bulut <umitbulut1993@hotmail.com>
2024-05-20 18:46:46 -0400

    First Bean and Configuretion has created  - 02/20/24 - 06:46 PM

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/RandomConfig.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/RandomConfig.java
@@ -1,18 +1,21 @@
 package com.cydeo.bean_annotation.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RandomConfig {
 
+
     @Bean
-    public String myString(){
-        return "Cydeo";
+    public String beanString(){
+        return "Bean String";
     }
 
     @Bean
-    public Integer myInteger(){
-        return 5;
+    public Integer beanInteger(){
+        return 1234;
     }
+
 }
